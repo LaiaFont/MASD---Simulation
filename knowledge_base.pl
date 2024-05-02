b1789c4c0a9ef1f75824593b1f4e1917a84e7159
Laia <laiafontsolanich@gmail.com>
2024-05-02 17:03:21 +0200

    Add more files for setup

--- knowledge_base.pl
+++ knowledge_base.pl
@@ -1,1 +1,16 @@
- 
+:- dynamic on/3, scaped/1 .
+
+% Add the amount of houses.
+house(1) .
+
+% Rooms in a house.
+room(1, kitchen) .
+room(1, bedroom) .
+room(1, bathroom) .
+
+% Add agents.
+agent(citizen1) .
+agent(citizen2) .
+
+% Danger.
+danger(fire) .
